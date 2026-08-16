563e8439f06ac83f73f00f5e294d8370909262e1
ThanuraSilva <thanurasilva1989@gmail.com>
2026-08-16 10:10:20 +0530

    Add nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 
 import { Intro } from './components/Intro';
+import { NavBar } from './components/navbar/NavBar';
 import { SignUp } from './components/User/SignUp';
 import { UserEdit } from './components/User/UserEdit';
 import { UserView } from './components/User/UserView';
 
 function App() {
   return (
-    // <Intro/>
-    // <SignUp/>
+    <>
+    <NavBar/>
     <UserView/>
-    // <UserEdit/>
+    </>
   )
 }
 
