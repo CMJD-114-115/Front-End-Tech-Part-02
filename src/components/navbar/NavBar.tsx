79750fdc0dbf54dac29a8ecd409c65f72da0111e
ThanuraSilva <thanurasilva1989@gmail.com>
2026-08-16 13:58:12 +0530

    Handle secure routes

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -3,10 +3,15 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import path from 'path'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { useAuth } from '../auth/AuthProvider'
+import { NavItem } from '../../models/NavItem'
 
-const navigation = [
-    { name: 'SignIn', path:"/sign-in", current: true },
+
+const openRoutes: NavItem[] = [
+    { name: 'SignIn', path: "/sign-in", current: true },
     { name: 'SignUp', path: "/sign-up", current: false },
+]
+
+const secureRoutes = [
     { name: 'Users', path: "/users", current: false },
     { name: 'UserActions', path: "/user-action", current: false },
     { name: 'Goles', path: "/goles", current: false },
@@ -20,14 +25,14 @@ function classNames(...classes: string[]) {
 export function NavBar() {
     const { isAuthenticated, logout } = useAuth()
     const navigate = useNavigate()
-    const handleOnLogOut = () =>{
+    const handleOnLogOut = () => {
         logout()
         navigate("/sign-in")
     }
     return (
         <Disclosure
             as="nav"
-            className="relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
+            className="relative bg-green-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
         >
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
@@ -43,61 +48,104 @@ export function NavBar() {
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div className="flex shrink-0 items-center">
                             <img
-                                alt="Your Company"
-                                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                                className="h-8 w-auto"
+                                alt="Nav Logo"
+                                src="https://cdn-icons-png.flaticon.com/512/11307/11307410.png"
+                                className="h-11 w-auto"
                             />
                         </div>
-                        <div className="hidden sm:ml-6 sm:block">
-                            <div className="flex space-x-4">
-                                {navigation.map((item) => (
-                                    <NavLink
-                                        key={item.name}
-                                        to={item.path}
-                                        aria-current={item.current ? 'page' : undefined}
-                                        className={classNames(
-                                            item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
-                                            'rounded-md px-3 py-2 text-sm font-medium',
-                                        )}
-                                    >
-                                        {item.name}
-                                    </NavLink>
-                                ))}
+                        {isAuthenticated ?
+                            <div className="hidden sm:ml-6 sm:block">
+                                <div className="flex space-x-4">
+                                    {secureRoutes.map((item) => (
+                                        <NavLink
+                                            key={item.name}
+                                            to={item.path}
+                                            aria-current={item.current ? 'page' : undefined}
+                                            className={classNames(
+                                                item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                                                'rounded-md px-3 py-2 text-sm font-medium',
+                                            )}
+                                        >
+                                            {item.name}
+                                        </NavLink>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
+
+                            :
+                            <div className="hidden sm:ml-6 sm:block">
+                                <div className="flex space-x-4">
+                                    {openRoutes.map((item) => (
+                                        <NavLink
+                                            key={item.name}
+                                            to={item.path}
+                                            aria-current={item.current ? 'page' : undefined}
+                                            className={classNames(
+                                                item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                                                'rounded-md px-3 py-2 text-sm font-medium',
+                                            )}
+                                        >
+                                            {item.name}
+                                        </NavLink>
+                                    ))}
+                                </div>
+                            </div>
+                        }
                     </div>
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <button
-                            type="button"
-                            className="relative rounded-full p-1 text-yellow-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
-                            onClick={handleOnLogOut}
-                        >
-                            {/* <span className="absolute -inset-1.5" />
+                        {isAuthenticated &&
+                            <button
+                                type="button"
+                                className="relative rounded-full p-1 text-yellow-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
+                                onClick={handleOnLogOut}
+                            >
+                                {/* <span className="absolute -inset-1.5" />
                             <span className="sr-only">View notifications</span>
                             <BellIcon aria-hidden="true" className="size-6" /> */}
-                            Logout
-                        </button>
+                                Logout
+                            </button>
+                        }
                     </div>
                 </div>
             </div>
 
             <DisclosurePanel className="sm:hidden">
-                <div className="space-y-1 px-2 pt-2 pb-3">
-                    {navigation.map((item) => (
-                        <DisclosureButton
-                            key={item.name}
-                            as={NavLink}
-                            to={item.path}
-                            aria-current={item.current ? 'page' : undefined}
-                            className={classNames(
-                                item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
-                                'block rounded-md px-3 py-2 text-base font-medium',
-                            )}
-                        >
-                            {item.name}
-                        </DisclosureButton>
-                    ))}
-                </div>
+                {isAuthenticated ? 
+                    <div className="space-y-1 px-2 pt-2 pb-3">
+                        {secureRoutes.map((item) => (
+                            <DisclosureButton
+                                key={item.name}
+                                as={NavLink}
+                                to={item.path}
+                                aria-current={item.current ? 'page' : undefined}
+                                className={classNames(
+                                    item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                                    'block rounded-md px-3 py-2 text-base font-medium',
+                                )}
+                            >
+                                {item.name}
+                            </DisclosureButton>
+                        ))}
+                    </div> 
+                    :
+                    <div className="space-y-1 px-2 pt-2 pb-3">
+                        {openRoutes.map((item) => (
+                            <DisclosureButton
+                                key={item.name}
+                                as={NavLink}
+                                to={item.path}
+                                aria-current={item.current ? 'page' : undefined}
+                                className={classNames(
+                                    item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                                    'block rounded-md px-3 py-2 text-base font-medium',
+                                )}
+                            >
+                                {item.name}
+                            </DisclosureButton>
+                        ))}
+                    </div>
+            
+            }
             </DisclosurePanel>
         </Disclosure>
     )
