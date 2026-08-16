126a609cb4a48e82f9830d666d7d0c68bd692962
ThanuraSilva <thanurasilva1989@gmail.com>
2026-08-16 11:47:31 +0530

    Login init

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@
 import { Route, Routes } from 'react-router-dom';
 import { Intro } from './components/Intro';
 import { NavBar } from './components/navbar/NavBar';
-import { SignUp } from './components/User/SignUp';
+import { SignUp } from './components/auth/SignUp';
 import { UserEdit } from './components/User/UserEdit';
 import { UserView } from './components/User/UserView';
+import { LogIn } from './components/auth/LogIn';
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
+        <Route path='/' element={<LogIn />} />
         <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<LogIn />} />
         <Route path='/users' element={<UserView />} />
       </Routes>
 
